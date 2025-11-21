422a42d2a872438354150bd01c6185fb4206e6c1
dutabrahmana <dutalinggau25@gmail.com>
2025-11-21 10:35:58 +0700

    menambahkan variabel di form siswa

--- app/src/main/java/com/example/mvvm/view/FormSiswa.kt
+++ app/src/main/java/com/example/mvvm/view/FormSiswa.kt
@@ -19,6 +19,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -31,8 +36,13 @@ import com.example.mvvm.R
 @Composable
 fun FormIsian(
     jenisK:List<String> = listOf("Laki-Laki","Perempuan"),
-    OnSubmitBtnClick : () -> Unit
+    onSubmitBtnClick : (MutableList<String>) -> Unit,
+    modifier: Modifier = Modifier
 ) {
+    var txtNama by rememberSaveable { mutableStateOf(value="") }
+    var txtAlamat by remember { mutableStateOf(value="") }
+    var txtGender by remember { mutableStateOf(value="") }
+    val listData: MutableList<String> = mutableListOf(txtNama,txtGender,txtAlamat)
     Scaffold(
         modifier = Modifier,
         {
