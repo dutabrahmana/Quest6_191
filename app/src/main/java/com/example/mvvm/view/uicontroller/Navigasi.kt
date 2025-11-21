3baed0803a1cffed577a006e2bb879583242ab14
dutabrahmana <dutalinggau25@gmail.com>
2025-11-21 12:52:38 +0700

    memperbaiki dan menyelesaikan Navigasi.kt

--- app/src/main/java/com/example/mvvm/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/mvvm/view/uicontroller/Navigasi.kt
@@ -1,43 +1,57 @@
 package com.example.mvvm.view.uicontroller
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.mvvm.model.DataJK.JenisK
 import com.example.mvvm.view.FormIsian
 import com.example.mvvm.view.TampilData
+import com.example.mvvm.viewmodel.SiswaViewModel
 
-enum class Navigation {
-    FormulirKu,
+
+enum class Navigasi {
+    Formulirku,
     Detail
 }
 
 @Composable
-fun DataApp(
+fun SiswaApp(
     navController: NavHostController = rememberNavController(),
-    modifier: Modifier = Modifier
-) {
-    Scaffold { isiRuang ->
+    modifier: Modifier,
+    viewModel: SiswaViewModel = viewModel ()
+){
+    Scaffold { isiRuang->
+        val uiState = viewModel.statusUI.collectAsState()
         NavHost(
             navController = navController,
-            startDestination = Navigation.FormulirKu.name,
-            modifier = Modifier.padding(paddingValues = isiRuang)
-        ) {
-            composable(route = Navigation.FormulirKu.name) {
-                FormIsian(
-                    OnSubmitBtnClick = {
-                        navController.navigate(route = Navigation.Detail.name)
+            startDestination = Navigasi.Formulirku.name,
+
+            modifier = Modifier.padding(isiRuang)){
+            composable(route = Navigasi.Formulirku.name){
+                val konteks = LocalContext.current
+                FormIsian (
+                    // Hanya melewatkan List ID resource: List<Int>
+                    pilihanJK = JenisK, // <-- Perubahan di sini
+                    onSubmitBtnClick = {
+                        viewModel.setSiswa(it)
+                        navController.navigate(Navigasi.Detail.name)
                     }
                 )
             }
-            composable(route = Navigation.Detail.name) {
-                TampilData(
+            composable(route = Navigasi.Detail.name){
+                TampilData (
+                    statusUiSiswa = uiState.value,
                     onBackBtnClick = {
-                        cancelAndBackToFormulirKu(navController)
+                        cancelAndBackToFormulirku(navController)
                     }
                 )
             }
@@ -45,11 +59,9 @@ fun DataApp(
     }
 }
 
-private fun cancelAndBackToFormulirKu(
+private fun cancelAndBackToFormulirku(
     navController: NavHostController
 ) {
-    navController.popBackStack(
-        route = Navigation.FormulirKu.name,
-        inclusive = false
-    )
+    navController.popBackStack(Navigasi.Formulirku.name,
+        inclusive = false)
 }
