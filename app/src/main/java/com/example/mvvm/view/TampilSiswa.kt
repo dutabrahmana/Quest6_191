f62d6ad480257b9f874ab709b700943682e739e1
dutabrahmana <dutalinggau25@gmail.com>
2025-11-21 10:58:44 +0700

    menambahkan edit di file tampilan siswa

--- app/src/main/java/com/example/mvvm/view/TampilSiswa.kt
+++ app/src/main/java/com/example/mvvm/view/TampilSiswa.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
@@ -25,74 +26,62 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.mvvm.R
+import com.example.mvvm.model.Siswa
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilData(
-    onBackBtnClick: () -> Unit
-) {
+    statusUiSiswa: Siswa,
+    onBackBtnClick:()->Unit
+){
     val items = listOf(
-        Pair(
-            first = stringResource(id = R.string.nama_lengkap),
-            second = "Contoh Nama"
-        ),
-        Pair(
-            first = stringResource(id = R.string.jenis_kelamin),
-            second = "Lainnya"
-        ),
-        Pair(
-            first = stringResource(id = R.string.alamat),
-            second = "Yogyakarta"
-        )
+        Pair(first = stringResource(id = R.string.nama_lengkap), second = statusUiSiswa.nama),
+        Pair(first = stringResource(id = R.string.jenis_kelamin), second = statusUiSiswa.gender),
+        Pair(first = stringResource(id = R.string.alamat), second = statusUiSiswa.alamat)
     )
-
-    Scaffold(
-        modifier = Modifier,
+    Scaffold (modifier = Modifier,
         topBar = {
             TopAppBar(
                 title = {
                     Text(
-                        text = stringResource(id = R.string.tampil),
+                        text = stringResource(id=R.string.tampil),
                         color = Color.White
+                    )},
+                colors = TopAppBarDefaults
+                    .mediumTopAppBarColors(colorResource
+                        (id = R.color.purple_500)
                     )
-                },
-                colors = TopAppBarDefaults.mediumTopAppBarColors(
-                    containerColor = colorResource(id = R.color.teal_700)
-                )
             )
-        }
-    ) { isiRuang ->
+        }){     isiRuang->
         Column(
-            modifier = Modifier.padding(paddingValues = isiRuang),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally
+            modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween
         ) {
             Column(
-                modifier = Modifier.padding(all = dimensionResource(id = R.dimen.padding_medium)),
-                verticalArrangement = Arrangement.spacedBy(space = dimensionResource(id = R.dimen.padding_small))
-
+                modifier = Modifier.padding(dimensionResource(id=R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy(dimensionResource
+                    (R.dimen.padding_small))
             ) {
                 items.forEach { item ->
                     Column {
-                        Text(text = item.first.uppercase(), fontSize = 16.sp)
-                        Text(
-                            text = item.second,
+                        Text(text = item.first.uppercase(),
+                            fontSize = 16.sp)
+                        Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_small)))
+                        Text(text = item.second,
                             fontWeight = FontWeight.Bold,
-                            fontFamily = FontFamily.Cursive,
-                            fontSize = 22.sp
-                        )
+                            fontFamily = FontFamily.Cursive, fontSize = 22.sp)
                     }
-                    HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
+                    Divider(thickness = dimensionResource(R.dimen.thickness_divider))
+                }
+                Spacer(modifier = Modifier.height(10.dp))
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = onBackBtnClick
+                ) {
+                    Text(text = stringResource(id=R.string.back))
                 }
             }
-
-            Spacer(modifier = Modifier.height(10.dp))
-            Button(
-                modifier = Modifier.fillMaxWidth(),
-                onClick = onBackBtnClick
-            ) {
-                Text(text = stringResource(id = R.string.back))
-            }
         }
+
     }
 }
